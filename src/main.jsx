78dd7d69b77321d03b740f816e0d419ded099c3c
Papataur <104798610+Papataur@users.noreply.github.com>
2026-05-27 19:58:16 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
-import { Analytics } from '@vercel/analytics/react';
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './style.css'
+import { Analytics } from '@vercel/analytics/react'
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>,
+)
