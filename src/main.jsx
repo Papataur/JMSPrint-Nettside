fe57dc696c548d308853112c3f0ca65b15f3072e
John-Marius Svendsen <laban1987@gmail.com>
2026-05-29 00:41:55 +0200

    Fix main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./style.css";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById("root")).render(<App />);
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './style.css'
-import { Analytics } from '@vercel/analytics/react'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Analytics />
-  </React.StrictMode>,
-)
->>>>>>> 78dd7d69b77321d03b740f816e0d419ded099c3c
+  </React.StrictMode>
+);
