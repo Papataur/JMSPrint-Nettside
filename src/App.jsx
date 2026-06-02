fab229e26e761b86972422e537fcfa2c9b54250f
Papataur <104798610+Papataur@users.noreply.github.com>
2026-06-02 23:01:27 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -217,7 +217,6 @@ export default function App() {
         <div className="shopGrid">
           <ProductItem
             image={salttrakt}
-            blueImage={salttraktBlue}
             title="Salttrakt til oppvaskmaskin"
             price="69 kr + frakt"
             text="Gjør påfylling av oppvasksalt enklere uten søl. Printet i slitesterk PETG."
