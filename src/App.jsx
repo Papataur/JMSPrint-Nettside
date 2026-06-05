87e24849591e27014ab6ddcc0b2e3228a4ceb518
Papataur <104798610+Papataur@users.noreply.github.com>
2026-06-05 21:31:18 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import deskholderWhite from "./deskholder-white.jpg";
 import holderpink from "./holderpink.jpg";
 import holderpinkBlack from "./holderpink-black.jpg";
 import holderpinkWhite from "./holderpink-white.jpg";
-import holderpinkPink from "./holderpink-pink.jpg";
+import holderpinkPink from "./holderpink.jpg";
 import holderpinkBlue from "./holderpink-blue.jpg";
 
 import salttraktWhite from "./salttrakt.jpg";
