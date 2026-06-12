43223514bd541e7241706f4718e05c8dc31f2812
Papataur <104798610+Papataur@users.noreply.github.com>
2026-06-12 19:58:50 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App.jsx";
+import Admin from "./Admin.jsx";
+
 import "./style.css";
 import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-    <Analytics />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+      <Analytics />
+    </BrowserRouter>
   </React.StrictMode>
 );
