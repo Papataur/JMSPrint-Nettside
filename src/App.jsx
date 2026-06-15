3e6468dd2615608eabb5f01be9b65b5847d8618a
Papataur <104798610+Papataur@users.noreply.github.com>
2026-06-15 19:57:34 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -548,15 +548,16 @@ export default function App() {
 
     try {
       const response = await fetch("/api/create-checkout-session", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-        total: totalPrice,
-        customer,
-        cart,
-       }),
+      method: "POST",
+      headers: {
+      "Content-Type": "application/json",
+     },
+      body: JSON.stringify({
+      total: totalPrice,
+      customer,
+      cart,
+     }),
+   });
 
       const result = await response.json();
 
