5c29f58f76c52108c0f25f8514f6104fadc37ddf
Papataur <104798610+Papataur@users.noreply.github.com>
2026-06-17 21:05:21 +0200

    Update confirm-vipps-payment.js

--- api/confirm-vipps-payment.js
+++ api/confirm-vipps-payment.js
@@ -1,1 +1,39 @@
+import { createClient } from "@supabase/supabase-js";
 
+const supabase = createClient(
+  process.env.VITE_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
+export default async function handler(req, res) {
+  try {
+    const { orderNumber } = req.query;
+
+    if (!orderNumber) {
+      return res.status(400).json({
+        error: "Mangler ordrenummer",
+      });
+    }
+
+    const { error } = await supabase
+      .from("orders")
+      .update({
+        status: "Betalt",
+      })
+      .eq("order_number", orderNumber);
+
+    if (error) {
+      throw error;
+    }
+
+    return res.redirect(
+      `https://www.jmsprint.no/?vipps=success&order=${orderNumber}`
+    );
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+}
